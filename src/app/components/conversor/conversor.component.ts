b67d891b7178afd83c0a0e0b1d7b626025537bf3
Gerardo Aramayo <leon.capo2000@gmail.com>
2026-05-24 23:20:18 -0300

    se agrego los estilos para todas las consignas

--- src/app/components/conversor/conversor.component.ts
+++ src/app/components/conversor/conversor.component.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-conversor',
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './conversor.component.html',
   styleUrl: './conversor.component.css',
 })
@@ -15,35 +15,7 @@ export class ConversorComponent implements OnInit {
 
   }
   moneda: any = {};
-  // monedaPrueba: any = {
-  //   "success": true,
-  //   "currencies": {
-  //     "AED": "United Arab Emirates Dirham",
-  //     "AFN": "Afghan Afghani",
-  //     "ALL": "Albanian Lek",
-  //     "AMD": "Armenian Dram",
-  //     "ANG": "Netherlands Antillean Guilder",
-  //     "AOA": "Angolan Kwanza",
-  //     "ARS": "Argentine Peso",
-  //     "USD": "United States Dollar",
-  //     "GBP": "British Pound Sterling",
-  //   }
-  // }
-  // convertidoPrueba: any = {
-  //   "date": "2005-01-01",
-  //   "historical": true,
-  //   "info": {
-  //     "quote": 0.51961,
-  //     "timestamp": 1104623999
-  //   },
-  //   "query": {
-  //     "amount": 10,
-  //     "from": "USD",
-  //     "to": "GBP"
-  //   },
-  //   "result": 5.1961,
-  //   "success": true
-  // }
+
   convertido: any = {};
   origen: string = "";
   destino: string = "";
@@ -54,16 +26,16 @@ export class ConversorComponent implements OnInit {
       (monedaData) => {
         this.moneda = monedaData;
       })
-    // this.moneda=this.monedaPrueba;
-  }
-  // cargarListaMonedas(){
 
-  // }
+  }
+
+
+
   conversion() {
     this.listaMonedas.convertirMonedas(this.cantidad, this.origen, this.destino).subscribe(
       (conversionData) => {
         this.convertido = conversionData;
       })
-    // this.convertido=this.convertidoPrueba;
+
   }
 }
